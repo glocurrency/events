ed0c2e3394fd50b2fb4a0aba31d0c48385b3770f
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-08-12 11:10:55 +0200

    feat: payments and compliance

--- group.go
+++ group.go
@@ -2,7 +2,9 @@ package events
 
 const (
 	BankingAuthEvents        = "banking-events-auth"
+	BankingComplianceEvents  = "banking-events-compliance"
 	BankingAccountEvents     = "banking-events-accounts"
 	BankingTransactionEvents = "banking-events-transactions"
+	BankingPaymentEvents     = "banking-events-payments"
 	BankingFxEvents          = "banking-events-fx"
 )
