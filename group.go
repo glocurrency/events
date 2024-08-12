8798fa9c5d862fd2788c0a3b244836527f5c3522
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-08-12 11:22:44 +0200

    feat!: change groups

--- group.go
+++ group.go
@@ -1,10 +1,23 @@
 package events
 
+// Events posted by the auth services.
 const (
-	BankingAuthEvents        = "banking-events-auth"
-	BankingComplianceEvents  = "banking-events-compliance"
-	BankingAccountEvents     = "banking-events-accounts"
-	BankingTransactionEvents = "banking-events-transactions"
-	BankingPaymentEvents     = "banking-events-payments"
-	BankingFxEvents          = "banking-events-fx"
+	AuthUserEvents = "auth-events-user"
+)
+
+// Events posted by the compliance services.
+const (
+	CompliancePaymentEvents = "compliance-events-payment"
+)
+
+// Events posted by the bank services.
+const (
+	BankAccountEvents     = "bank-events-account"
+	BankTransactionEvents = "bank-events-transaction"
+	BankFxEvents          = "bank-events-fx"
+)
+
+// Events posted by the banking service.
+const (
+	BankingPaymentEvents = "banking-events-payment"
 )
