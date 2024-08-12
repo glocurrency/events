7275e78d7aa81e527b80a44704364fe376047487
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-08-12 16:36:37 +0200

    fix!: payment accepted

--- compliance/payment.go
+++ compliance/payment.go
@@ -11,22 +11,29 @@ const PaymentPendingEvent = "compliance-payment-pending"
 const PaymentApprovedEvent = "compliance-payment-approved"
 const PaymentDeclinedEvent = "compliance-payment-declined"
 
+type PaymentAccepted struct {
+	PaymentID          uuid.UUID
+	ComplianceProvider string
+	Reason             string
+	Timestamp          time.Time
+}
+
 type PaymentPending struct {
-	PaymentID          uuid.NullUUID
+	PaymentID          uuid.UUID
 	ComplianceProvider string
 	Reason             string
 	Timestamp          time.Time
 }
 
 type PaymentApproved struct {
-	PaymentID          uuid.NullUUID
+	PaymentID          uuid.UUID
 	ComplianceProvider string
 	Reason             string
 	Timestamp          time.Time
 }
 
 type PaymentDeclined struct {
-	PaymentID          uuid.NullUUID
+	PaymentID          uuid.UUID
 	ComplianceProvider string
 	Reason             string
 	Timestamp          time.Time
