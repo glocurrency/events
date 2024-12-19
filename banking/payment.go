03d0fbe8d9067492841ddb9cda503900d2babadb
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-12-19 23:52:20 +0000

    feat: more types

--- banking/payment.go
+++ banking/payment.go
@@ -16,9 +16,18 @@ type DocumentMedia struct {
 	Content  []byte
 }
 
+type DocumentType string
+
+const (
+	DocumentTypePassport        DocumentType = "PASSPORT"
+	DocumentTypeIDCard          DocumentType = "ID_CARD"
+	DocumentTypeDriversLicense  DocumentType = "DRIVERS_LICENSE"
+	DocumentTypeResidencePermit DocumentType = "RESIDENCE_PERMIT"
+)
+
 type IdentificationDocument struct {
 	Country    string
-	Type       string
+	Type       DocumentType
 	Number     string
 	ValidFrom  *time.Time
 	ValidUntil *time.Time
@@ -35,9 +44,42 @@ const (
 type Profession string
 
 const (
-	ProfessionAgriculture Profession = "AGRICULTURE"
-	ProfessionRetired     Profession = "RETURED"
-	// TODO: add more professions
+	ProfessionAgriculture    Profession = "AGRICULTURE"
+	ProfessionArchitect      Profession = "ARCHITECT"
+	ProfessionDefence        Profession = "DEFENCE"
+	ProfessionArtist         Profession = "ARTIST"
+	ProfessionAntiquesTrader Profession = "ANTIQUES_TRADER"
+	ProfessionAssistantNurse Profession = "ASSISTANT_NURSE"
+	ProfessionBanking        Profession = "BANKING"
+	ProfessionBusinessAdmin  Profession = "BUSINESS_ADMIN"
+	ProfessionCarDealer      Profession = "CAR_DEALER"
+	ProfessionCivilServant   Profession = "CIVIL_SERVANT"
+	ProfessionCleaning       Profession = "CLEANING"
+	ProfessionAdminOfficer   Profession = "ADMIN_OFFICER"
+	ProfessionConstruction   Profession = "CONSTRUCTION"
+	ProfessionDiplomat       Profession = "DIPLOMAT"
+	ProfessionFactoryWorker  Profession = "FACTORY_WORKER"
+	ProfessionTransport      Profession = "TRANSPORT"
+	ProfessionGambling       Profession = "GAMBLING"
+	ProfessionFitness        Profession = "FITNESS"
+	ProfessionHealthcare     Profession = "HEALTHCARE"
+	ProfessionHospitality    Profession = "HOSPITALITY"
+	ProfessionJewelry        Profession = "JEWELRY"
+	ProfessionJudge          Profession = "JUDGE"
+	ProfessionLegal          Profession = "LEGAL"
+	ProfessionMerchant       Profession = "MERCHANT"
+	ProfessionMilitary       Profession = "MILITARY"
+	ProfessionMining         Profession = "MINING"
+	ProfessionRetired        Profession = "RETIRED"
+	ProfessionPharma         Profession = "PHARMA"
+	ProfessionPolitics       Profession = "POLITICS"
+	ProfessionSecurity       Profession = "SECURITY"
+	ProfessionAthlete        Profession = "ATHLETE"
+	ProfessionRealEstate     Profession = "REAL_ESTATE"
+	ProfessionReligious      Profession = "RELIGIOUS"
+	ProfessionTaxiDriver     Profession = "TAXI_DRIVER"
+	ProfessionTeaching       Profession = "TEACHING"
+	ProfessionTobacco        Profession = "TOBACCO"
 )
 
 type PaymentComplianceRequired struct {
